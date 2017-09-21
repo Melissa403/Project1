ef74a30502b856de5e35a54bea6d2a1bc978c9d2
Melissa <melissa403@gmail.com>
2017-09-20 21:02:49 -0500

    Update mel_java.js
    Added incomplete form alert

--- mel_java.js
+++ mel_java.js
@@ -1,4 +1,23 @@
+function myForm(form) {
+	var userName = form.name.value;
+	var userEmail = form.email.value;
+	var success = 1;
 
-function myFunction() {
-    alert("Thank you for your submission!");
+	if(!userName) {
+		success =0 ;
+	}
+
+	if(!userEmail) {
+		success = 0;
+	}
+
+	if(!success) {
+		alert("Oops! Looks like you left something out!");
+		return false;
+	}
+
+	else{
+		alert("Get ready for panda facts!");
+		return true;
+	}
 }
